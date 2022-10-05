1cc7a35709b9004a6f3694376ae6b039e39a2ca5
UBC Student <brettm01@students.cs.ubc.ca>
2022-10-05 10:59:57 -0700

    refactored structor because previous method required access to internet - deleted unnecessary classes and began creating stubs for Image class)

--- src/main/model/Image.java
+++ src/main/model/Image.java
@@ -1,18 +1,43 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
 
+// represents an instance of the locally stored image with a list of applied filters
 public class Image {
+    List<String> listOfFilter;
+    int[][] pixels;
 
-    FilterList filterList;
-
-    //EFFECTS: instantiates an Image object with an empty filterList field
-    public Image() {
-        // stub
-    }
-
-    //EFFECTS: applies all filters to image object to create new edited image
-    public void processImage(){
+    //TODO: create blank canvas by converting 2D array to array of randomly colored pixels
+    //REQUIRES: width, height > 0
+    //EFFECTS: creates Image object with 2D pixel array with (width) columns, (height) rows, and
+    //         empty listOfFilter;
+    public Image(int width, int height) {
         //stub
     }
+
+    //TODO: maybe scratch add noise if each new image canvas is already randomized
+    //REQUIRES: filter string must be one of "pixelate", "negative", "add noise", "mirror"
+    //MODIFIES: this
+    //EFFECTS: adds filter to an images listOfFilter field
+    public void addFilter(String filter) {
+        //stub
+    }
+
+    //REQUIRES: listOfFilter is not empty
+    //MODIFIES: this
+    //EFFECTS: removes the most recently added filter from listOfFilter
+    public void undo() {
+        //stub
+    }
+
+    //REQUIRES:
+    //MODIFIES:
+    //EFFECTS:
+    public void undoAll() {
+        //stub
+    }
+
 }
+
+
