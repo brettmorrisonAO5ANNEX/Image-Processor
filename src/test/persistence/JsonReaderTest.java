5c85e637ff29f77d98eca5911b42cc822b49ecb4
UBC Student <brettm01@students.cs.ubc.ca>
2022-10-18 21:17:55 -0700

    added test cases for JsonReader and JsonWriter relating to an invalid file

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,4 +1,32 @@
 package persistence;
 
+import model.Image;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class JsonReaderTest {
+
+    @Test
+    public void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noFile.json");
+        try {
+            Image img = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            //pass
+        }
+    }
+
+    @Test
+    void testReaderNewImage() {
+        //stub
+    }
+
+    @Test
+    void testReaderPartiallyEditedImage() {
+        //stub
+    }
 }
--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -1,4 +1,33 @@
 package persistence;
 
+import model.Image;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class JsonWriterTest {
+
+    @Test
+    void testWriterInvalidFile() {
+        try {
+            Image img = new Image(1, 1);
+            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
+            writer.open();
+            fail("IOException was expected");
+        } catch (FileNotFoundException e) {
+           //pass
+        }
+    }
+
+    @Test
+    void testWriterNewImage() {
+        //stub
+    }
+
+    @Test
+    void testWriterPartiallyEditedImage() {
+        //stub
+    }
 }
