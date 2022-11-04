1b0ac70aedade80ebcc29a506522a6087d70d28a
UBC Student <brettm01@students.cs.ubc.ca>
2022-11-04 11:41:01 -0700

    created helpers to reduce OpenPanel() method length and debugged button layout issues

--- src/main/ui/OpenPanel.java
+++ src/main/ui/OpenPanel.java
@@ -10,71 +10,89 @@ public class OpenPanel extends JPanel {
     private JPanel logoPanel;
     private JPanel optionPanel;
 
-    private JFrame openFrame;
+    private JButton newImage;
+    private JButton loadPrev;
+    private JButton viewGall;
+
+    private JFrame mainFrame;
 
     //EFFECTS: creates an opening panel with logo and options for user ot create new, laod previous, or view gallery
     //         (for the last two options, buttons should be un-clickable if no current projects or no gallery projects)
     public OpenPanel() {
-        //constraints object used to set constraints for each button in optionPanel
+        mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createEmptyBorder());
+        mainPanel.setLayout(new GridLayout(0,1));
+
+        logoPanel = new JPanel();
+        logoPanel.setBorder(BorderFactory.createEmptyBorder());
+        logoPanel.setLayout(new GridLayout(0, 1));
+        mainPanel.add(logoPanel);
+
+        optionPanel = new JPanel();
+        optionPanel.setBorder(BorderFactory.createEmptyBorder());
+        optionPanel.setLayout(new GridBagLayout());
+        mainPanel.add(optionPanel);
+
+        createButtons(optionPanel);
+
+        mainFrame = new JFrame();
+        mainFrame.setTitle("image.(in)");
+        mainFrame.setDefaultCloseOperation(mainFrame.EXIT_ON_CLOSE);
+        mainFrame.add(mainPanel);
+        mainFrame.setVisible(true);
+        mainFrame.pack();
+        mainFrame.setBounds(200, 200, 500, 300);
+    }
+
+    //MODIFIES: this
+    //EFFECTS: creates all new, load previous, and view gallery buttons
+    public void createButtons(JPanel optionPanel) {
+        createNewImageButton(optionPanel);
+        createLoadPrevButton(optionPanel);
+        createViewGallButton(optionPanel);
+    }
+
+    //MODIFIES: this
+    //EFFECTS: creates button for creating new project with specified constraints for GridBagLayout
+    public void createNewImageButton(JPanel optionPanel) {
         GridBagConstraints c = new GridBagConstraints();
 
-        //test to see if logo dimension will be ok
-        JButton testLogo = new JButton("Image.(in)");
-
-        JButton newImage = new JButton("New Project");
+        newImage = new JButton("New Project");
         c.fill = GridBagConstraints.HORIZONTAL;
-        c.ipadx = 10;
-        c.ipady = 10;
+//        c.ipadx = 10;
+//        c.ipady = 10;
         c.gridwidth = 2;
         c.gridx = 0;
         c.gridy = 0;
 
-        JButton loadPrev = new JButton("Load Project");
+        optionPanel.add(newImage, c);
+    }
+
+    //MODIFIES: this
+    //EFFECTS: creates load previous button with specified constraints
+    public void createLoadPrevButton(JPanel optionPanel) {
+        GridBagConstraints c = new GridBagConstraints();
+
+        loadPrev = new JButton("Load Project");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridwidth = 1;
         c.gridx = 0;
         c.gridy = 1;
 
-        JButton viewGall = new JButton("View Gallery");
+        optionPanel.add(loadPrev, c);
+    }
+
+    //MODIFIES: this
+    //EFFECTS: creates view gallery button with specified constraints
+    public void createViewGallButton(JPanel optionPanel) {
+        GridBagConstraints c = new GridBagConstraints();
+
+        viewGall = new JButton("View Gallery");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridwidth = 1;
         c.gridx = 1;
         c.gridy = 1;
 
-        logoPanel = new JPanel();
-        logoPanel.setBorder(BorderFactory.createEmptyBorder());
-        logoPanel.setLayout(new GridLayout(0, 1));
-        //test to see if logo dimension will be ok
-        logoPanel.add(testLogo);
-
-        optionPanel = new JPanel();
-        optionPanel.setBorder(BorderFactory.createEmptyBorder());
-        optionPanel.setLayout(new GridBagLayout());
-        // create constraints to keep new above load and view (but view and load shoule be next to eachother
-        optionPanel.add(newImage);
-        optionPanel.add(loadPrev);
-        optionPanel.add(viewGall);
-
-        mainPanel = new JPanel();
-        mainPanel.setBorder(BorderFactory.createEmptyBorder());
-        mainPanel.setLayout(new GridLayout(0,1));
-        mainPanel.add(logoPanel);
-        mainPanel.add(optionPanel);
-
-
-//        openPanel = new JPanel();
-//        openPanel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
-//        openPanel.setLayout(new GridLayout(0, 1));
-//        openPanel.add(newImage);
-//        openPanel.add(loadPrev);
-//        openPanel.add(viewGall);
-//
-//        openFrame = new JFrame();
-//        openFrame.setBounds(100, 100, 100, 100);
-//        openFrame.setTitle("Image.(in)");
-//        openFrame.setDefaultCloseOperation(openFrame.EXIT_ON_CLOSE);
-//        openFrame.setVisible(true);
-//        openFrame.setSize(400, 400);
-//        openFrame.add(openPanel);
+        optionPanel.add(viewGall, c);
     }
 }
