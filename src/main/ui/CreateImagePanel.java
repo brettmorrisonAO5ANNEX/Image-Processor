d8bd7428fa7474b72254a253f32b168d30a603ba
UBC Student <brettm01@students.cs.ubc.ca>
2022-11-12 15:19:10 -0800

    added exception handling for dimension input when user is deciding width and height of project

--- src/main/ui/CreateImagePanel.java
+++ src/main/ui/CreateImagePanel.java
@@ -1,10 +1,13 @@
 package ui;
 
 import model.Image;
+import model.exceptions.InvalidInputException;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 //TODO: add code credit to oracle
 //represents panel that is shown when user is creating a new project
@@ -23,7 +26,7 @@ public class CreateImagePanel extends JPanel {
         super();
         setBorder(BorderFactory.createEmptyBorder());
         setLayout(new GridBagLayout());
-        createChooseNameField();
+//        createChooseNameField();
         createChooseDimensions();
         createRandomizeChooser();
         createConfirm();
@@ -31,19 +34,26 @@ public class CreateImagePanel extends JPanel {
         this.iaGUI = iaGUI;
     }
 
-    //MODIFIES: this
-    //EFFECTS: creates text field element so user can set project name according to regex specifications
-    private void createChooseNameField() {
-        GridBagConstraints c = new GridBagConstraints();
-        JTextField chooseName = new JTextField(10);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 2;
-        c.gridx = 0;
-        c.gridy = 0;
-
-        add(chooseName, c);
-    }
+//    //INSPIRATION SOURCE:
+//    //https://stackoverflow.com/questions/11200585/adding-a-prompt-text-property-to-jtextfield
+//    //MODIFIES: this
+//    //EFFECTS: creates text field element so user can set project name according to regex specifications
+//    private void createChooseNameField() {
+//        String promptText = "choose name";
+//        String errorMessage = "name does not meet requirements";
+//        GridBagConstraints c = new GridBagConstraints();
+//        JTextField chooseName = new JTextField(promptText);
+//        JLabel errorLabel = createErrorLabel(1, 0, 0, errorMessage);
+//
+//        createListener(chooseName, promptText, errorLabel);
+//
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        c.gridwidth = 2;
+//        c.gridx = 0;
+//        c.gridy = 1;
+//
+//        add(chooseName, c);
+//    }
 
     //MODIFIES: this
     //EFFECTS: creates text boxes for dimensions setting and adds them to main layout
@@ -55,9 +65,13 @@ public class CreateImagePanel extends JPanel {
     //MODIFIES: this
     //EFFECTS: creates width text field and adds it to main layout
     private void createWidthField() {
+        String promptText = "width (px)";
+        String errorText = "invalid input";
         GridBagConstraints c = new GridBagConstraints();
+        JLabel errorLabel = createErrorLabel(1, 0, 0, errorText);
+        chooseWidth = new JTextField(promptText, 10);
 
-        chooseWidth = new JTextField(10);
+        createListener(chooseWidth, promptText, errorLabel);
 
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridwidth = 1;
@@ -70,8 +84,13 @@ public class CreateImagePanel extends JPanel {
     //MODIFIES: this
     //EFFECTS: creates width text field and adds it to layout
     private void createHeightField() {
+        String promptText = "height (px)";
+        String errorText = "invalid input";
         GridBagConstraints c = new GridBagConstraints();
-        chooseHeight = new JTextField(10);
+        JLabel errorLabel = createErrorLabel(1, 1, 0, errorText);
+        chooseHeight = new JTextField(promptText, 10);
+
+        createListener(chooseHeight, promptText, errorLabel);
 
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridwidth = 1;
@@ -85,8 +104,7 @@ public class CreateImagePanel extends JPanel {
     //EFFECTS: creates combo box dropdown so user can choose to randomize project color
     public void createRandomizeChooser() {
         GridBagConstraints c = new GridBagConstraints();
-
-        String[] chooseRandomizeOptions = {"Choose An Option", "yes", "no"};
+        String[] chooseRandomizeOptions = {"", "yes", "no"};
 
         ActionListener comboBoxListener = e -> {
             JComboBox cb = (JComboBox) e.getSource();
@@ -138,5 +156,86 @@ public class CreateImagePanel extends JPanel {
             myImage.randomizeColor();
         }
         return myImage;
+    }
+
+    //INSPIRATION SOURCE FOR PROMPT TEXT:
+    //https://stackoverflow.com/questions/11200585/adding-a-prompt-text-property-to-jtextfield
+    //MODIFIES: this
+    //EFFECTS: creates focus listener for text fields that displays prompt text that disappears when user
+    //         clicks on desired textField
+    private void createListener(JTextField textField, String promptText, JLabel errorLabel) {
+        textField.setForeground(Color.GRAY);
+        errorLabel.setVisible(false);
+
+        FocusListener listener = new FocusListener() {
+
+            @Override
+            public void focusGained(FocusEvent e) {
+                performFocused(textField, promptText, errorLabel);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                performFocusLost(textField, promptText, errorLabel);
+            }
+        };
+
+        textField.addFocusListener(listener);
+    }
+
+    //MODIFIES: this
+    //EFFECTS: removes prompt text and updates input color when user is engaging with textField
+    private void performFocused(JTextField textField, String promptText, JLabel errorLabel) {
+        if (textField.getText().equals(promptText)) {
+            textField.setText("");
+            textField.setForeground(Color.BLACK);
+        }
+        // for error handling
+        textField.setBackground(Color.WHITE);
+        errorLabel.setVisible(false);
+    }
+
+    //MODIFIES: this
+    //EFFECTS: adds prompt text if needed and handles input exceptions
+    private void performFocusLost(JTextField textField, String promptText, JLabel errorLabel) {
+        if (textField.getText().isEmpty()) {
+            textField.setText(promptText);
+            textField.setForeground(Color.GRAY);
+        }
+        // for error handling
+        try {
+            validateContents(textField);
+        } catch (InvalidInputException i) {
+            textField.setBackground(new Color(255, 166, 166));
+            errorLabel.setVisible(true);
+        }
+    }
+
+    //MODIFIES: this
+    //EFFECTS: creates and places error JLabel with corresponding error message
+    private JLabel createErrorLabel(int gridWidth, int gridx, int gridy, String errorMessage) {
+        GridBagConstraints c = new GridBagConstraints();
+        JLabel errorLabel = new JLabel(errorMessage);
+        errorLabel.setForeground(Color.RED);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = gridWidth;
+        c.gridx = gridx;
+        c.gridy = gridy;
+
+        add(errorLabel, c);
+        return errorLabel;
+    }
+
+    //MODIFIES: this
+    //EFFECTS: checks contents of text field and throws exception if invalid
+    private void validateContents(JTextField textField) throws InvalidInputException {
+        String contents = textField.getText();
+
+        try {
+            Integer.parseInt(contents);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException();
+        }
     }
 }
